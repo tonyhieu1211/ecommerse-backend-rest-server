7e8c7bedb5461135e06cb07a0d41a8e1b4362983
tran minh hieu <tranminhhieu9901@gmail.com>
2021-03-20 00:28:36 +0700

    remove multer file upload

--- src/routes/page.js
+++ src/routes/page.js
@@ -1,13 +1,10 @@
 const { createPage, getPage } = require('../controllers/page');
-const { upload, requireSignin, adminMiddleware } = require('../middleware');
+const { requireSignin, adminMiddleware } = require('../middleware');
 
 const express = require('express');
 const router = express.Router();
 
-router.post('/page/create',requireSignin,adminMiddleware, upload.fields([
-    {name:'banners'},
-    {name:'products'}
-]), createPage);
+router.post('/page/create',requireSignin,adminMiddleware,createPage);
 
 router.get(`/page/:category/:type`, getPage);
 
